4da63cbdb2c7dd05cadd6375830ec8faa5bc7434
user <user@localhost>
2026-02-24 01:12:41 +0400

    Added println statement

--- Spring2026_LA1_Repo/src/Driver.java
+++ Spring2026_LA1_Repo/src/Driver.java
@@ -6,7 +6,8 @@ public class Driver {
 		System.out.println("Ayman Khalid");
 		System.out.println("YASER SAFAR"); //
 		System.out.println("mahmoud "); //
-		System.out.println("Ataa Mohammed Keaied"); //
+		System.out.println("Ataa Mohammed Keaied");//
+		System.out.println("Luma Alhasan");
 	}
 
 }
